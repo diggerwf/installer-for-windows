3c87ab0fbd885d3f09f336cbe08b03636afa9f4c
diggerwf <80178091+diggerwf@users.noreply.github.com>
2026-01-18 19:21:44 +0100

    Update installe.bat

--- installe.bat
+++ installe.bat
@@ -2,8 +2,8 @@
 setlocal enabledelayedexpansion
 
 :: --- KONFIGURATION ---
-set "REPO_URL=https://github.com/diggerwf/installer-for-windows.git"
-set "BRANCH=beta-1"
+set "REPO_URL=https://github.com/USER/PROJEKT.git"
+set "BRANCH=main"
 set "START_FILE=start.bat"
 :: ---------------------
 
@@ -28,82 +28,60 @@ cd /d "%TARGET_DIR%"
 git --version >nul 2>&1
 if %errorlevel% neq 0 (
     echo [+] Git wird installiert...
-    [cite_start]winget install --id Git.Git -e --source winget --accept-package-agreements --accept-source-agreements [cite: 2]
+    winget install --id Git.Git -e --source winget --accept-package-agreements --accept-source-agreements [cite: 2]
     set "PATH=%PATH%;C:\Program Files\Git\cmd"
 )
 
 :PROCESS
-set "DIR_NAME="
-
+:: Falls bereits eine .git Datei da ist, nur updaten [cite: 3]
 if exist ".git" (
-    [cite_start]echo [+] Bestehendes Projekt gefunden. [cite: 3]
-    [cite_start]echo Pruefe auf Updates... [cite: 3]
+    echo [+] Bestehendes Projekt gefunden. Pruefe auf Updates... [cite: 3]
     git remote set-url origin "!REPO_URL!"
-    [cite_start]git -c credential.helper= fetch origin %BRANCH% --quiet [cite: 4]
+    git -c credential.helper= fetch origin %BRANCH% --quiet [cite: 4]
     
     for /f "tokens=*" %%a in ('git rev-parse HEAD') do set "LOCAL_HASH=%%a"
     for /f "tokens=1" %%a in ('git ls-remote origin %BRANCH%') do set "REMOTE_HASH=%%a"
     
     if "!LOCAL_HASH!"=="!REMOTE_HASH!" (
-        [cite_start]echo [+] Alles aktuell. [cite: 5]
+        echo [+] Alles aktuell. [cite: 5]
     ) else (
-        echo [+] Update verfuegbar. Lade neue Daten...
-        git pull origin %BRANCH%
+        echo [+] Update verfuegbar. Lade neue Daten... [cite: 5]
+        git pull origin %BRANCH% [cite: 5]
     )
 ) else (
-    echo [+] Kein Projekt gefunden. Starte Neu-Installation...
-    
-    set "IS_EMPTY=YES"
-    dir /b /a | findstr . >nul 2>&1
-    if %errorlevel% equ 0 set "IS_EMPTY=NO"
-
-    if "!IS_EMPTY!"=="YES" (
-        echo [+] Ordner ist leer. Klone Branch %BRANCH%...
-        git -c credential.helper= clone -b %BRANCH% %REPO_URL% .
-    ) else (
-        [cite_start]for %%F in ("%REPO_URL%") do set "DIR_NAME=%%~nF" [cite: 6]
-        echo [!] Ordner nicht leer. Klone in Unterordner: !DIR_NAME!
+    :: Prüfung ob Ordner leer ist
+    dir /a /b | findstr . >nul 2>&1
+    if %errorlevel% equ 0 (
+        :: Ordner NICHT leer -> Unterordner erstellen und reingehen
+        for %%F in ("%REPO_URL%") do set "DIR_NAME=%%~nF" 
+        echo [+] Ordner nicht leer. Erstelle Unterordner: !DIR_NAME!
         
+        :: Klone direkt in den Unterordner
         git -c credential.helper= clone -b %BRANCH% %REPO_URL% "!DIR_NAME!"
-        
-        if exist "!DIR_NAME!" (
-            cd "!DIR_NAME!"
-        )
+        if exist "!DIR_NAME!" cd /d "!DIR_NAME!"
+    ) else (
+        :: Ordner IST leer -> direkt hierher klonen
+        echo [+] Klone Branch %BRANCH%...
+        git -c credential.helper= clone -b %BRANCH% %REPO_URL% .
     )
 )
 
 :START_LOGIC
 echo.
-echo --- Startvorgang ---
-
-:: 1. Prüfung im aktuellen Verzeichnis
+echo --- Pruefe Start-Datei ---
 if exist "%START_FILE%" (
-    echo [+] %START_FILE% gefunden. Starte...
+    echo [+] %START_FILE% gefunden. Starte jetzt...
     echo -------------------------------------------
     call "%START_FILE%"
-    goto :END
+    exit
+) else (
+    echo.
+    echo [!] FEHLER: Die Datei "%START_FILE%" konnte nicht gefunden werden.
+    echo [i] Aktueller Pfad: %CD%
+    echo [i] Inhalt dieses Ordners:
+    dir /b
+    echo.
+    echo Druecke eine belibige Taste um zu beenden
+    pause
+    exit
 )
-
-:: 2. Prüfung im Unterordner (falls vorhanden)
-if defined DIR_NAME (
-    if exist "!DIR_NAME!\%START_FILE%" (
-        echo [+] Datei im Unterordner "!DIR_NAME!" gefunden.
-        cd "!DIR_NAME!"
-        echo -------------------------------------------
-        call "%START_FILE%"
-        goto :END
-    )
-)
-
-:: 3. Fehlerfall: Datei ist nicht da
-echo.
-echo [!] FEHLER: Die Datei "%START_FILE%" konnte nicht gefunden werden.
-echo [i] Inhalt des aktuellen Ordners:
-dir /b /a-d
-echo.
-echo Druecke eine belibige Taste um zu beenden
-pause
-exit
-
-:END
-exit
